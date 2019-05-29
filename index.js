8c5abf55809181733ef778a38892fb747be3bb7f
Andrew Mariano <41345622+corimstyle@users.noreply.github.com>
2019-05-29 00:07:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix, token } = require('./config.json');
+const prefix = "!corim";
 
 client.once('ready', () => {
     console.log('Ready!');
